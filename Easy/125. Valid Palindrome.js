f310731ee671ae4ed84beeba4c533ae83cfcb410
Joon Hwang <joonhwangs@gmail.com>
2023-01-30 19:51:51 -0500

    easy - 125. valid palindrome

---
+++ Easy/125. Valid Palindrome.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+  s = s.toLowerCase().replace(/[^a-z0-9]/gi,'');
+  let start = 0;
+  let end = s.length - 1;
+  while (start < end) {
+      if(s[start] !== s[end]) {
+          return false;
+      }
+      start++;
+      end--;
+  }
+  return true;
+};
