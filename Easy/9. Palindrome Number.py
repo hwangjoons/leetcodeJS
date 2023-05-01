c507f038fb1578086c976e1b77ac4bbbcf2b965b
Joon Hwang <joonhwangs@gmail.com>
2023-04-30 23:57:07 -0400

    easy 9 - palindrome number.PY

---
+++ Easy/9. Palindrome Number.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0 or (x > 0 and x % 10 == 0):
+            return False
+
+        result = 0
+
+        while x > result:
+            result = result * 10 + x % 10
+            x /= 10
+
+        return True if (x == result or x == result // 10) else False
