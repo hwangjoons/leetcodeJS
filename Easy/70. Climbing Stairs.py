ea8f8cc9bc4fa5187a6966f69ea1ce936bc27190
Joon Hwang <joonhwangs@gmail.com>
2023-05-16 22:54:08 -0400

    easy - 70. climbing stairs. PY

---
+++ Easy/70. Climbing Stairs.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        def climb(n):
+            #edge cases
+            if n==0: return 0
+            if n==1: return 1
+            if n==2: return 2
+            dp = [0]*(n+1) # considering zero steps we need n+1 places
+            dp[1]= 1
+            dp[2] = 2
+            for i in range(3,n+1):
+                dp[i] = dp[i-1] +dp[i-2]
+            print(dp)
+            return dp[n]
+
