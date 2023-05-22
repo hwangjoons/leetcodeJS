bb0a027214c9b079af2a6d26809360e13195fabf
Joon Hwang <joonhwangs@gmail.com>
2023-05-21 23:26:58 -0400

    easy - 217. contains duplicate.PY

---
+++ Easy/217. Contains Duplicate.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        hset = set()
+        for idx in nums:
+            if idx in hset:
+                return True
+            else:
+                hset.add(idx)
