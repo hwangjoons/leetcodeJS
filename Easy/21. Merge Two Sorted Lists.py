4cf8a5ec3c8d5c0a93bcd8a3ababa8a21f68ee79
Joon Hwang <joonhwangs@gmail.com>
2023-05-30 23:50:36 -0400

    easy - 21. merge two sorted lists.PY

---
+++ Easy/21. Merge Two Sorted Lists.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        if not list1 or not list2:
+          return list1 or list2
+
+        if list1.val < list2.val:
+          list1.next = self.mergeTwoLists(list1.next, list2)
+          return list1
+        else:
+          list2.next = self.mergeTwoLists(list1, list2.next)
+          return list2
