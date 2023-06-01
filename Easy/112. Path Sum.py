f43575b819437fb94afdf90c401f7bdcbfb90559
Joon Hwang <joonhwangs@gmail.com>
2023-05-31 23:10:52 -0400

    easy - 112. path sum.PY

---
+++ Easy/112. Path Sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    # @param root, a tree node
+    # @param sum, an integer
+    # @return a boolean
+    # 1:27
+    def hasPathSum(self, root, sum):
+        if not root:
+            return False
+
+        if not root.left and not root.right and root.val == sum:
+            return True
+
+        sum -= root.val
+
+        return self.hasPathSum(root.left, sum) or self.hasPathSum(root.right, sum)
